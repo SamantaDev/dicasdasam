d939802a641d8bb78fbe9581f1474949b678e855
SamantaDev <97115786+SamantaDev@users.noreply.github.com>
2026-08-01 23:33:55 -0300

    Adiciona rotas do painel administrativo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,70 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+
+import Admin from "./pages/Admin/Admin";
+import Login from "./pages/Admin/Login";
+import Dashboard from "./pages/Admin/Dashboard";
+import AdminProducts from "./pages/Admin/Products/Products";
+import NewProduct from "./pages/Admin/NewProduct/NewProduct";
+import Categories from "./pages/Admin/Categories/Categories";
+
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+
+      <Routes>
+
+        {/* Site público */}
+        <Route path="/" element={<Home />} />
+
+        <Route 
+          path="/produtos" 
+          element={<Products />} 
+        />
+
+        <Route 
+          path="/produto/:id" 
+          element={<Product />} 
+        />
+
+
+        {/* Área administrativa */}
+        <Route 
+          path="/admin" 
+          element={<Admin />} 
+        />
+
+        <Route 
+          path="/admin/login" 
+          element={<Login />} 
+        />
+
+        <Route 
+          path="/admin/dashboard" 
+          element={<Dashboard />} 
+        />
+
+        <Route 
+          path="/admin/products" 
+          element={<AdminProducts />} 
+        />
+
+        <Route 
+          path="/admin/new-product" 
+          element={<NewProduct />} 
+        />
+
+        <Route 
+          path="/admin/categories" 
+          element={<Categories />} 
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+  );
 }
